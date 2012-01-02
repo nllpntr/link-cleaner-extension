96d7b5ac866ef38f342021cd12e8657e0b53dd68
Mark Raddatz <mraddatz@gmail.com>
2012-01-02 21:05:18 +0800

    Fix not working for every external link

--- content.js
+++ content.js
@@ -2,10 +2,11 @@ setInterval(function() {
     var links = document.getElementsByTagName("a");
     for(var i = 0; i < links.length; i++) {
         var link = links[i];
-        if (link.rel === "nofollow" && link.onmousedown !== null) {
+        if (link.rel.indexOf("nofollow") !== -1 && link.target == "_blank") {
             var a = link.cloneNode();
             a.innerHTML = link.innerHTML;
             a.removeAttribute("onmousedown");
+            a.setAttribute("rel", "cleaned");
             a.setAttribute("style", "color: #3D983D;");
             link.parentNode.replaceChild(a, link);
         }
